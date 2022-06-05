2b76f780465e2d2ee013693efb23dcf265326868
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-05 08:27:06 -0600

    Get countries using the region is ready. Reset the selected country when the region changes is working.

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/countries.interface';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -9,12 +12,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SelectorPageComponent implements OnInit {
   myForm: FormGroup = this.formBuilder.group({
-    region: ['', Validators.required]
+    region: ['', Validators.required],
+    country: ['', Validators.required]
   })
+  regions: string[] = [];
+  countries: Country[] = [];
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private countriesService: CountriesService) { }
 
   ngOnInit(): void {
+    this.regions = this.countriesService.regions;
+    
+    this.myForm.get('region')?.valueChanges.pipe(
+      tap(() => {
+        this.myForm.get('country')?.reset('');
+      }),
+      switchMap(country => this.countriesService.getCountriesByRegion(country))
+    ).subscribe({
+      next: (countries: Country[]) => {
+        this.countries = countries
+      }
+    })
   }
 
   save(): void {}
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/countries.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
+  private _baseURL = 'https://restcountries.com/v3.1';
 
-  constructor() { }
+  private _regions: string[] = [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  get regions(): string[]{
+    return [...this._regions]
+  }
+
+  constructor(private http: HttpClient) { }
+
+  getCountriesByRegion(region: string): Observable<Country[]> {
+    return this.http.get<Country[]>(`${this._baseURL}/region/${region}?fields=name,cca3`)
+  }
 }
